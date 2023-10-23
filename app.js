39473c7c10671dd4877d3f99f5d61f88416eca57
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-10-23 13:19:12 +0300

    add .env

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const DB_HOST =
-//   "mongodb+srv://alex:Lh6LPYjEcAtm28Ar@cluster0.e6tf29l.mongodb.net/books_reader?retryWrites=true&w=majority";
-
-// mongoose.set("strictQuery", true);
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connected"))
-//   .catch((error) => console.log(error.message));
-
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const { DB_HOST } = process.env;
-
+console.log(DB_HOST);
 mongoose.set("strictQuery", true);
 
 mongoose
