ef2a6820ee47adbb66a9aed22e98d7308ad5ccc0
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-10-23 12:03:32 +0300

    bf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require("./app");
 const DB_HOST =
   "mongodb+srv://alex:Lh6LPYjEcAtm28Ar@cluster0.e6tf29l.mongodb.net/books_reader?retryWrites=true&w=majority";
 
-mongoose.set("strictQuery"), true;
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
